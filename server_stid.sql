10df5360e2d08b651a222fb7dfd3b2cc3dc12f5c
yohan apedo <yohan.apedo@etu.univ-poitiers.fr>
2022-11-21 16:04:45 +0100

    premiere version

--- server_stid.sql
+++ server_stid.sql
@@ -1,5 +1,9 @@
+<<<<<<< 1829a2adb2b8e7d2555957724655455ab0b0cc4d
 --TP SQL
 --Exercice 2 : requêtes simples
 select * 
 from MOVIE 
 where DIRECTOR = 'Steven Spielberg';i
+=======
+--TP SQL
+>>>>>>> premiere version
